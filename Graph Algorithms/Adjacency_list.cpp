c5354b5aa5b80f8ac4abdb73c569eb199ec4e2dd
“Sushmoy <learnsushmoysk1091@gmail.com>
2024-12-14 20:25:38 +0600

    using weight

--- Graph Algorithms/Adjacency_list.cpp
+++ Graph Algorithms/Adjacency_list.cpp
@@ -1,24 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 1e5 + 5;
-vector<int> adj[N];
+vector<pair<int, int>> adj[N];
 int main()
 {
     int n, m;
     cin >> n >> m;
     for (int i = 0; i < m; i++)
     {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        int u, v, w;
+        cin >> u >> v>>w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u,w});
     }
     for (int i = 1; i <= n; i++)
     {
         cout << "List " << i << ": ";
-        for (int j : adj[i])
+        for (auto j : adj[i])
         {
-            cout << j << " ";
+            cout << "(" << j.first;
+            cout << ", " << j.second << ")";
         }
         cout << endl;
     }
