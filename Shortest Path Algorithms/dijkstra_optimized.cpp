278d18fd072e56ed8c3ab38b59f3bf0159c2a044
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-01-11 23:25:07 +0600

    Bellman Ford Algorithm added

--- Shortest Path Algorithms/dijkstra_optimized.cpp
+++ Shortest Path Algorithms/dijkstra_optimized.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 const int N = 1e5 + 5;
 vector<pair<int, int>> v[N];
 int dis[N];
 bool vis[N];
 void dijkstra(int s)
 {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-    pq.push({0, s});
+    priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>pq;
+    pq.push({0,s});
     dis[s] = 0;
     while (!pq.empty())
     {
-        pair<int, int> parent = pq.top();
+        pair<int,int>parent=pq.top();
         pq.pop();
         int parentNode = parent.second;
-        if (vis[parentNode])
+        if(vis[parentNode])
         {
             continue;
         }
-        vis[parentNode] = true;
-        int parentCost = parent.first;
+        vis[parentNode]=true;
+        int parentCost=parent.first;
         for (int i = 0; i < v[parentNode].size(); i++)
         {
             pair<int, int> child = v[parentNode][i];
@@ -29,7 +29,7 @@ void dijkstra(int s)
             if (parentCost + childCost < dis[childNode])
             {
                 dis[childNode] = parentCost + childCost;
-                pq.push({dis[childNode], childNode});
+                pq.push({dis[childNode],childNode});
             }
         }
     }
@@ -43,16 +43,18 @@ int main()
         int a, b, w;
         cin >> a >> b >> w;
         v[a].push_back({b, w});
-        // v[b].push_back({a, w});
+        //v[b].push_back({a, w});
     }
     for (int i = 1; i <= n; i++)
     {
         dis[i] = INT_MAX;
     }
     dijkstra(1);
-    for (int i = 1; i <= n; i++)
+
+    for(int i=1;i<=n;i++)
     {
-        cout << "Node " << i << ": " << dis[i] << endl;
+        cout<<"Node "<<i<<": "<<dis[i]<<endl;
     }
+
     return 0;
 }
